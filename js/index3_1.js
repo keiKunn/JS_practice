92b63419fca3c7d3c0325874b44b7fc68183a1b4
keiKunn <k.aragn03@gmail.com>
2020-11-29 08:52:40 +0900

    Javascript課題➂-4 実施

--- js/index3_1.js
+++ js/index3_1.js
@@ -6,6 +6,11 @@ const tasks = [];
 const taskTable = document.getElementById('task_table_id');
 // 追加ボタン押下イベント
 document.getElementById('addBtn').addEventListener('click', addTask);
+// ラジオボタン表示切替イベント
+const radioElements = document.getElementsByName('todo_radio_name');
+radioElements.forEach(element => {
+  element.addEventListener('click', changeDisplay);
+});
 
 /**
 * タスク追加機能
@@ -18,51 +23,80 @@ function addTask() {
   }
   // 入力値をタスクオブジェクトに追加
   const newtask = {
-    id: !tasks.length ? 0 : taskTable.rows.length - 1,
+    id: !tasks.length ? 0 : tasks.length,
     comment: inputTask,
     status: '作業中'
   }
   // 追加タスクをタスクオブジェクト格納配列に追加
   tasks.push(newtask);
   // タスク表示
-  displayTask(tasks);
+  changeDisplay();
 }
 
 /**
 * タスク状態変更機能
 *  index 行数
 */
-function changeStatus(index) {
-  const oldStatus = tasks[index].status;
+function changeStatus(id) {
+  const oldStatus = tasks[id].status;
   // taskオブジェクトの状態を変更
   if (oldStatus === '作業中') {
-    tasks[index].status = '完了';
+    tasks[id].status = '完了';
   } else {
-    tasks[index].status = '作業中';
+    tasks[id].status = '作業中';
   }
   // ボタン表示切替
-  const targetBtn = document.getElementById(`workBtn_${index}`);
-  targetBtn.innerText = tasks[index].status;
+  const targetBtn = document.getElementById(`workBtn_${id}`);
+  targetBtn.innerText = tasks[id].status;
+  // タスク表示
+  changeDisplay();
+}
+
+/**
+* タスク表示切替機能
+*/
+function changeDisplay() {
+  // ラジオボタンDOM取得
+  const allRadio = document.getElementById('all_radio_id');
+  const workRadio = document.getElementById('work_radio_id');
+  const doneRadio = document.getElementById('done_radio_id');
+
+  // 表示切替
+  if(allRadio.checked){
+    displayTask(tasks);
+  } else if (workRadio.checked) {
+    const workTasks = tasks.filter(task => {
+      return task.status === '作業中';
+    })
+    // 作業中タスクを表示
+    displayTask(workTasks);
+  } else if (doneRadio.checked) {
+    const doneTasks = tasks.filter(task => {
+      return task.status === '完了';
+    })
+    // 作業中タスクを表示
+    displayTask(doneTasks);
+  }
 }
 
 /**
 * タスク削除機能
-*  index 行数
+*  id 
 */
-function deleteTask(index) {
+function deleteTask(id) {
   // tasksオブジェクトから削除
-  tasks.splice(index, 1);
+  tasks.splice(id, 1);
   // tasksオブジェクトのid値を振り直し
-  reAssignTaskId(tasks);
+  reAssignTaskId();
   // タスク表示
-  displayTask(tasks);
+  changeDisplay();
 }
 
 /**
 *タスク表示
-* tasks 
+* arrayTasks タスクオブジェクト格納配列
 */
-function displayTask(tasks) {
+function displayTask(arrayTasks) {
   // 追加済みタスク行が表示されている場合、削除
   const taskLength = taskTable.rows.length;
   if (taskLength > 1 ) {
@@ -74,7 +108,7 @@ function displayTask(tasks) {
   }
 
   // タスク行生成
-  tasks.forEach((obj, index) => {
+  arrayTasks.forEach(obj => {
     // 行追加
     const taskRow = taskTable.insertRow(-1);
     // タスクid セット
@@ -89,20 +123,20 @@ function displayTask(tasks) {
     cell = taskRow.insertCell(-1);
     const workBtn = document.createElement('button')
     workBtn.type = 'button';
-    workBtn.id = `workBtn_${index}`;
+    workBtn.id = `workBtn_${obj.id}`;
     workBtn.innerText = obj.status;
     workBtn.addEventListener('click', () => {
-      changeStatus(index);
+      changeStatus(obj.id);
     });
     cell.appendChild(workBtn);
     // 削除ボタンセット
     cell = taskRow.insertCell(-1);
     const deleteBtn = document.createElement('button')
     deleteBtn.type = 'button';
-    deleteBtn.id = `deleteBtn_${index}`;
+    deleteBtn.id = `deleteBtn_${obj.id}`;
     deleteBtn.innerText = '削除';
     deleteBtn.addEventListener('click', () => {
-      deleteTask(index);
+      deleteTask(obj.id);
     });
     cell.appendChild(deleteBtn);
   });
@@ -110,9 +144,8 @@ function displayTask(tasks) {
 
 /**
 *タスクid 振り直し
-* tasks タスクオブジェクト格納配列
 */
-function reAssignTaskId(tasks) {
+function reAssignTaskId() {
   let cnt = 0;
   tasks.forEach(obj => {
     obj.id = cnt;
